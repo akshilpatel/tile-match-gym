d6e230ec3cdd5bf487eea7461cc5b870da97f3a1
akshilpatel <akshilpatel11@gmail.com>
2023-07-05 10:44:38 +0100

    Uncomment tests

--- tests/board/test_activation.py
+++ tests/board/test_activation.py
@@ -1,5 +1,5 @@
 import pytest
-from src.board import Board
+from src.tile_match_gym.board import Board
 import numpy as np
 from typing import Optional, List, Tuple
 from utils import print_board_diffs
@@ -9,25 +9,27 @@ from utils import print_board_diffs
 ################################################################################
 
 # Need to call 'pytest -k test_activation -s' to run the file and get output if error
+@pytest.fixture
+def t1():
 
+    t1 = {
+        "original": np.array([
+            [2,3,2,3,2,3,2],
+            [3,2,3,2,3,2,3],
+            [2,3,2,3,2,3,2],
+            [3,2,3,2,3,2,3],
+            [2,3,2,3,2,3,2],
+            ]),
+        "expected": np.array([
+            [2,3,2,3,2,3,2],
+            [3,2,3,2,3,2,3],
+            [2,3,2,3,2,3,2],
+            [3,2,3,2,3,2,3],
+            [2,3,2,3,2,3,2],
+            ]),
+    }
 
-t1 = {
-    "original": np.array([
-        [2,3,2,3,2,3,2],
-        [3,2,3,2,3,2,3],
-        [2,3,2,3,2,3,2],
-        [3,2,3,2,3,2,3],
-        [2,3,2,3,2,3,2],
-        ]),
-    "expected": np.array([
-        [2,3,2,3,2,3,2],
-        [3,2,3,2,3,2,3],
-        [2,3,2,3,2,3,2],
-        [3,2,3,2,3,2,3],
-        [2,3,2,3,2,3,2],
-        ]),
-}
-
+    yield t1
 
 
 def test_activation():
@@ -42,7 +44,3 @@ def test_activation():
     if not np.array_equal(board.board, t1["expected"]):
         print()
         print_board_diffs.highlight_board_diff(board.board, t1["expected"])
-
-
-
-
--- tests/board/test_translator.py
+++ tests/board/test_translator.py
@@ -1,34 +1,35 @@
-# import pytest
-# from src.board import Board, TileTranslator
-# import numpy as np
-# from typing import Optional, List, Tuple
-# from .utils import create_alternating_array, contains_threes
-# 
-# ################################################################################
-# ################## This is just templating - needs to be changed ###############
-# ################################################################################
-# 
-# @pytest.fixture
-# def board():
-#     example_board = Board(0,0,0,0,np.array([
-#         [2,3,2,3,2,3,2],
-#         [3,2,3,2,3,2,3],
-#         [2,3,2,3,2,3,2],
-#         [3,2,3,2,3,2,3],
-#         [2,3,2,3,2,3,2],
-#     ]))
-#     yield example_board
-# 
-# 
-# # @pytest.mark.parametrize("height, width, num_colours", [
-# #     (4, 4, 4),
-# #     (90, 2, 12),
-# #     (50, 50, 4),
-# #     (8, 8, 1),
-# # ])
-# # def (height: int, width:int, num_colours:int, seed:Optional[int] = None):
-# #     board = Board(height, width, num_colours, seed)
-# #     assert not contains_threes(board.board)
-# #     assert board.board.shape == (height, width)
-# #     assert board.num_colours == num_colours
-# # 
+import pytest
+from src.tile_match_gym.board import Board, TileTranslator
+import numpy as np
+from typing import Optional, List, Tuple
+from tests.board.utils2 import create_alternating_array, contains_threes
+
+################################################################################
+################## This is just templating - needs to be changed ###############
+################################################################################
+
+@pytest.fixture
+def board():
+    example_board = Board(0,0,0,0,np.array([
+        [2,3,2,3,2,3,2],
+        [3,2,3,2,3,2,3],
+        [2,3,2,3,2,3,2],
+        [3,2,3,2,3,2,3],
+        [2,3,2,3,2,3,2],
+    ]))
+    yield example_board
+
+
+@pytest.mark.parametrize("height, width, num_colours", [
+    (4, 4, 4),
+    (90, 2, 12),
+    (50, 50, 4),
+    (8, 8, 1),
+])
+
+def test_(height: int, width:int, num_colours:int, seed:Optional[int] = None):
+    board = Board(height, width, num_colours, seed)
+    assert not contains_threes(board.board)
+    assert board.board.shape == (height, width)
+    assert board.num_colours == num_colours
+
