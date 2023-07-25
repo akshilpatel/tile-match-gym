2e49f023381b8a6e90cb56fe8303ad8eee2832cc
James Elson <jameselson19@gmail.com>
2023-07-25 14:38:19 +0100

    Changed how certain tiles are registered, got 3 and 4 line activations done and tested and changed printing of board

--- tile_match_gym/tile_translator.py
+++ tile_match_gym/tile_translator.py
@@ -4,8 +4,9 @@ from typing import Tuple
 
 
 class TileTranslator:
-    def __init__(self, num_colours: int, board_shape: Tuple[int, int]):
+    def __init__(self, num_colours: int, board_shape: Tuple[int, int], num_specials: int = 4):
         self.num_colours = num_colours
+        self.num_specials = num_specials
         self.board_shape = board_shape
 
     def _tile_type_str(self, tile_idx: int):
@@ -20,6 +21,20 @@ class TileTranslator:
             return "bomb"
         elif tile_type == 4:
             return "cookie"
+        else:
+            raise ValueError("Invalid tile type.")
+
+    def tile_to_id(self, tile_type: str):
+        if tile_type == "norm":
+            return 1
+        elif tile_type == "vertical_laser":
+            return 2
+        elif tile_type == "horizontal_laser":
+            return 3
+        elif tile_type == "bomb":
+            return 4
+        elif tile_type == "cookie":
+            return 0
         else:
             raise ValueError("Invalid tile type.")
 
@@ -40,3 +55,30 @@ class TileTranslator:
 
     def get_tile_colour(self, tile_idx: int):
         return (tile_idx - 1) % self.num_colours
+
+    def get_tile_number(self, tile: str, colour: int):
+        """
+        Convert the tile type and colour to the tile index.
+
+        0 = empty
+        1 = cookie
+        2 = color1 norm
+        3 = color1 vertical laser
+        4 = color1 horizontal laser
+        5 = color1 bomb
+        6 = color2 norm
+        7 = color2 vertical laser
+        8 = color2 horizontal laser
+        9 = color2 bomb
+
+        equation -> color * num_specials + tile_type + 2
+        """
+
+        print("tile: ", tile)
+        type_encoding = self.tile_to_id(tile)
+
+        if tile == "cookie":
+            return 1
+        else:
+            return colour * self.num_specials + type_encoding + 1
+
