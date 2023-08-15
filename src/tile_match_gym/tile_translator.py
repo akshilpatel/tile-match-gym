82cbe5ede6c26de055c1a60e336b6fb331cc24ec
akshilpatel <akshilpatel11@gmail.com>
2023-08-15 16:14:23 +0100

    Fix translator to generalise to any number of colourless and coloured specials. Also add testing for this.

--- src/tile_match_gym/tile_translator.py
+++ src/tile_match_gym/tile_translator.py
@@ -1,115 +1,154 @@
 import numpy as np
 
-from typing import Tuple
+from typing import Tuple, List
+
+# 0 = empty
+# 1 = non_colour_special 0
+# ...
+# k = non_colour_special k
+# k+1 normal colour 0
+# ...
+# k+n+1 normal colour n
+# k+n+2 colour_special_1 colour 0
+# ...
+# k+2n+1 colour_special_1 colour n
+# k+2n+2 colour_special_2 colour 0
+# ...
 
 
 class TileTranslator:
-    def __init__(self, num_colours: int, board_shape: Tuple[int, int], num_specials: int = 4):
+    def __init__(
+        self,
+        num_colours: int,
+        board_shape: Tuple[int, int],
+        colourless_specials: List[str] = ["cookie"],
+        colour_specials: List[str] = ["vertical_laser", "horizontal_laser", "bomb"],
+    ):
         self.num_colours = num_colours
-        self.num_specials = num_specials
+        self.colour_specials = colour_specials
+        self.colourless_specials = colourless_specials
+        self.num_colourless_specials = len(colourless_specials)
+        self.num_colour_specials = len(colour_specials)
+        self.num_specials = self.num_colour_specials + self.num_colourless_specials
         self.board_shape = board_shape
-        self.type_names = ["none", "cookie", "norm", "vertical_laser", "horizontal_laser", "bomb"]
-    
-    def get_tile_encoding(self, tile: str, colour: int):
+        self.all_tile_types = ["none"] + self.colourless_specials + ["normal"] + self.colour_specials
+        self.max_tile_encoding = self.num_colourless_specials + (1 + self.num_specials) * self.num_colours
+
+    def get_tile_encoding(self, tile_type: str, tile_colour: int):
         """
         Convert the tile type and colour to the tile index.
-
         0 = none
-        1 = cookie
-        2 = color1 norm
-        3 = color1 vertical laser
-        4 = color1 horizontal laser
-        5 = color1 bomb
-        6 = color2 norm
-        7 = color2 vertical laser
-        8 = color2 horizontal laser
-        9 = color2 bomb
+        1 = colourless_special_0
+        2 = colourless_special_1
+        ...
+        k = colourless_special_k-1
+        k+1 = colour0 normal
+        ...
+        k+n+1 = colour0 vertical laser
+        ...
+        k+2n+1 = colour0 horizontal laser
+        ...
+        k+3n+1 = colour0 bomb
+        ...
+        k+4n = colourn-1 bomb
 
-        equation -> color * num_specials + tile_type + 2
         """
-        type_encoding = self.type_names.index(tile)
-        if tile == "none":
+        if not 0 <= tile_colour < self.num_colours:
+            raise ValueError(f"The tile colour {tile_colour} not in possible tile colours (0, {self.num_colours})")
+
+        elif tile_type not in self.all_tile_types:
+            raise ValueError(f"The tile type {tile_type} not in possible tile types {self.all_tile_types}")
+
+        if tile_type == "none":
             return 0
-        elif tile == "cookie":
-            return 1
+        elif tile_type in self.colourless_specials:
+            return self.colourless_specials.index(tile_type) + 1  # 1 -> num_non_colour_specials-1
+        elif tile_type == "normal":
+            return 1 + self.num_colourless_specials + tile_colour  # num_non_colour_specials -> num_non_colour_specials + num_colours + 1
         else:
-            return colour * self.num_specials + type_encoding - 4
+            type_idx = 1 + self.colour_specials.index(tile_type)  # To get past normal tiles, need to add 1.
+            return (type_idx * self.num_colours) + tile_colour + 1 + self.num_colourless_specials
 
-
-    def get_type_color(self, encoding: int) -> Tuple[int, int]:
+    def get_type_colour(self, encoding: int) -> Tuple[int, int]:
         """
         Convert the encoding to a tile type and colour.
 
         types:
-            0 = none
-            1 = cookie
-            2 = norm
-            3 = vertical laser
-            4 = horizontal laser
-            5 = bomb
+            none: 0
+            colourless_special_type_0: 1 (zero-based_indexing)
+            ...
+            colourless_special_type_k-1: k
+            norm: k+1
+            vertical laser: k+2
+            horizontal laser: k+3
+            bomb: k+4
         """
+        if not 0 <= encoding <= self.max_tile_encoding:
+            raise ValueError(f"The encoding {encoding} is not valid.")
 
         if encoding == 0:
             return 0, 0
-        elif encoding == 1:
-            return 1, 20
+        elif 0 < encoding <= self.num_colourless_specials:
+            return encoding, 0
         else:
-            return (encoding - 2) % self.num_specials + 2, (encoding - 2) // self.num_specials + 1
+            recentered_encoding = encoding - 1 - self.num_colourless_specials
+            tile_colour = recentered_encoding % self.num_colours
+            tile_type = (recentered_encoding // self.num_colours) + self.num_colourless_specials + 1
+            return tile_type, tile_colour
 
-    def is_same_color(self, encoding1: int, encoding2: int) -> bool:
+    def is_same_colour(self, encoding1: int, encoding2: int) -> bool:
         """
         Check if the two encodings are the same colour.
         """
-        _, c1 = self.get_type_color(encoding1)
-        _, c2 = self.get_type_color(encoding2)
+        _, c1 = self.get_type_colour(encoding1)
+        _, c2 = self.get_type_colour(encoding2)
         return c1 == c2
 
     def get_str(self, encoding: int) -> str:
         """
         Convert the encoding to a string.
         """
-        t, c = self.get_type_color(encoding)
+        t, c = self.get_type_colour(encoding)
 
         if t == 0:
             return "none"
-        elif t == 1:
-            return "cookie"
-        elif t == 2:
-            return "color{} norm".format(c)
-        elif t == 3:
-            return "color{} vertical laser".format(c)
-        elif t == 4:
-            return "color{} horizontal laser".format(c)
-        elif t == 5:
-            return "color{} bomb".format(c)
-        
-        return "?"
+        elif 0 < t <= self.num_colourless_specials:
+            return f"Colourless special: {self.num_colourless_specials[t - 1]}"
+        elif t == self.num_colour_specials + 1:
+            return f"Colour{c} norm"
+        elif t == self.num_colour_specials + 2:
+            return f"Colour{c} vertical laser"
+        elif t == self.num_colour_specials + 3:
+            return f"Colour{c} horizontal laser"
+        elif t == self.num_colour_specials + 4:
+            return f"Colour{c} bomb"
+        else:
+            raise NotImplementedError(f"The encoding {encoding} is not valid.")
 
     def get_char(self, encoding: int) -> str:
         """
         Convert the encoding to a character.
         """
-        t, _ = self.get_type_color(encoding)
+        # t, _ = self.get_type_colour(encoding)
 
-        if t == 0:
-            return "0"
-        elif t == 1:
-            return "o"
-        elif t == 2:
-            return str(encoding)
-        elif t == 3:
-            return "|"
-        elif t == 4:
-            return "-"
-        elif t == 5:
-            return "*"
-        
-        return "?"
+        # if t == 0:
+        #     return "0"
+        # elif t == 1:
+        #     return "o"
+        # elif t == 2:
+        #     return str(encoding)
+        # elif t == 3:
+        #     return "|"
+        # elif t == 4:
+        #     return "-"
+        # elif t == 5:
+        #     return "*"
+
+        raise NotImplementedError("This method should not exist.")
 
     def is_special(self, encoding: int) -> bool:
         """
         Check if the encoding is a special tile.
         """
-        t, _ = self.get_type_color(encoding)
-        return t != 2 and t != 0
-
+        t, _ = self.get_type_colour(encoding)
+        return not (t == 0 or t == self.num_colourless_specials + 1)
--- tests/board/test_translator.py
+++ tests/board/test_translator.py
@@ -20,12 +20,70 @@ from tests.utils import create_alternating_array, contains_threes
 #     yield example_board
 
 
-@pytest.mark.parametrize("height, width, num_colours", [
-    # (4, 4, 4),
-    (90, 2, 12),
-    # (50, 50, 4),
-    # (8, 8, 1),
-])
-def test_(height: int, width:int, num_colours:int, seed:Optional[int] = None):
-    board = Board(height, width, num_colours, seed)
-    
+def test_get_tile_encoding():
+    tile_translator = TileTranslator(
+        num_colours=4, board_shape=(5, 10), colourless_specials=["cookie"], colour_specials=["vertical_laser", "horizontal_laser", "bomb"]
+    )
+
+    # Empty tile
+    assert tile_translator.get_tile_encoding("none", 0) == 0
+    # Cookie
+    assert tile_translator.get_tile_encoding("cookie", 0) == 1
+
+    # Normal tiles
+    assert tile_translator.get_tile_encoding("normal", 0) == 2
+    assert tile_translator.get_tile_encoding("normal", 3) == 5
+
+    # Lasers
+    assert tile_translator.get_tile_encoding("vertical_laser", 3) == 9
+    assert tile_translator.get_tile_encoding("horizontal_laser", 1) == 11
+
+    tile_translator = TileTranslator(num_colours=7, board_shape=(5, 10), colourless_specials=[], colour_specials=["vertical_laser", "horizontal_laser", "bomb"])
+    assert tile_translator.get_tile_encoding("normal", 0) == 1
+    assert tile_translator.get_tile_encoding("normal", 4) == 5
+    assert tile_translator.get_tile_encoding("bomb", 6) == 28
+
+    tile_translator = TileTranslator(num_colours=34, board_shape=(12, 5), colourless_specials=[], colour_specials=[])
+    assert tile_translator.get_tile_encoding("normal", 0) == 1
+    assert tile_translator.get_tile_encoding("normal", 33) == 34
+    print(tile_translator.all_tile_types)
+    with pytest.raises(ValueError):
+        tile_translator.get_tile_encoding("bomb", 10)
+
+
+def test_get_type_color():
+    tile_translator = TileTranslator(
+        num_colours=4, board_shape=(5, 10), colourless_specials=["cookie"], colour_specials=["vertical_laser", "horizontal_laser", "bomb"]
+    )
+    assert tile_translator.get_type_colour(0) == (0, 0)
+    assert tile_translator.get_type_colour(1) == (1, 0)
+    assert tile_translator.get_type_colour(4) == (2, 2)
+    assert tile_translator.get_type_colour(12) == (4, 2)
+
+    tile_translator = TileTranslator(num_colours=5, board_shape=(5, 10), colourless_specials=["cookie"], colour_specials=["bomb"])
+    assert tile_translator.get_type_colour(0) == (0, 0)
+    assert tile_translator.get_type_colour(1) == (1, 0)
+    assert tile_translator.get_type_colour(4) == (2, 2)
+    assert tile_translator.get_type_colour(8) == (3, 1)
+
+    tile_translator = TileTranslator(num_colours=5, board_shape=(5, 10), colourless_specials=[], colour_specials=[])
+    assert tile_translator.get_type_colour(0) == (0, 0)
+    assert tile_translator.get_type_colour(1) == (1, 0)
+    assert tile_translator.get_type_colour(4) == (1, 3)
+    assert tile_translator.get_type_colour(5) == (1, 4)
+
+
+def test_is_special():
+    tile_translator = TileTranslator(
+        num_colours=4, board_shape=(5, 10), colourless_specials=["cookie"], colour_specials=["vertical_laser", "horizontal_laser", "bomb"]
+    )
+    assert not tile_translator.is_special(0)
+    assert tile_translator.is_special(1)
+    assert not tile_translator.is_special(4), tile_translator.get_type_colour(4)
+    assert tile_translator.is_special(12)
+
+    tile_translator = TileTranslator(num_colours=7, board_shape=(5, 10), colourless_specials=[], colour_specials=[])
+    assert not tile_translator.is_special(0)
+    assert not tile_translator.is_special(1)
+    assert not tile_translator.is_special(4)
+    assert not tile_translator.is_special(7)
