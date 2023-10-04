23c300c5bdd7fc2a4ed9172fbde7fb1e2f9d43ea
akshilpatel <akshilpatel11@gmail.com>
2023-10-04 16:31:03 +0100

    Add tests for _get_colour_lines

--- tests/board/test_match_detection.py
+++ tests/board/test_match_detection.py
@@ -4,55 +4,87 @@ import pytest
 from tile_match_gym.board import Board
 
 
-def make_board():
-    board = Board(8, 7, ["cookie"], ["vertical_laser", "horizontal_laser", "bomb"], 4)
-    return board
+def test_get_colour_lines():
+    b = Board(num_rows=3, num_cols=4, num_colours=3)
 
+    # Colourless specials should not be included
+    b.board = np.array(
+        [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+    )
+    assert b._get_colour_lines() == []
 
-@pytest.fixture
-def make_board_fixture():
-    return make_board()
+    b.board = np.array(
+        [
+            [1, 4, 4, 1],
+            [1, 1, 1, 1],
+            [1, 4, 4, 1],
+        ]
+    )
+    assert b._get_colour_lines() == []
 
+    # No lines
+    b.board = np.array([[4, 3, 4, 3], [5, 4, 5, 4], [4, 3, 4, 2]])
 
-# def test_get_lowest_v_match_coords():
-#     board = make_board_fixture
-#     board.board = np.array([
-#         [4, 4, 2, 1, 3, 2, 3], # 0
-#         [3, 1, 1, 1, 2, 3, 4], # 1
-#         [4, 4, 2, 1, 3, 2, 3], # 2
-#         [2, 3, 2, 2, 2, 3, 2], # 3
-#         [1, 1, 2, 3, 3, 3, 4], # 4
-#         [1, 3, 2, 4, 1, 2, 4], # 5
-#         [4, 3, 1, 3, 1, 4, 4], # 6
-#         [3, 3, 3, 1, 1, 1, 4], # 7
-#     ])
-#     print(board)
+    assert b._get_colour_lines() == [], b._get_colour_lines()
 
-#     board.board = np.array([
-#         [4, 4, 2, 2, 2, 2, 3], # 0
-#         [3, 1, 1, 2, 2, 3, 4], # 1
-#         [4, 4, 2, 1, 3, 2, 3], # 2
-#         [2, 3, 2, 2, 1, 3, 2], # 3
-#         [1, 1, 2, 3, 2, 3, 3], # 4
-#         [1, 3, 2, 4, 1, 2, 4], # 5
-#         [4, 3, 1, 3, 1, 4, 4], # 6
-#         [3, 2, 3, 2, 1, 1, 4], # 7
-#     ])
+    # Single vertical line on top edge
+    b.board[1, 0] = 4
+    assert b._get_colour_lines()[0] == [(0, 0), (1, 0), (2, 0)], b._ge_colour_lines()
+    assert len(b._get_colour_lines()) == 1
 
-#     print(board.get_lowest_h_match_coords())
+    # Single horizontal line on bottom edge.
+    b.board[1, 0] = 2
+    b.board[2, 1] = 4
+    assert b._get_colour_lines()[0] == [(2, 0), (2, 1), (2, 2)], b._get_colour_lines()
+    assert len(b._get_colour_lines()) == 1
 
-#     board.board = np.array([
-#         [4, 4, 4, 3, 2, 2, 2], # 0
-#         [3, 1, 1, 2, 2, 3, 4], # 1
-#         [4, 4, 2, 1, 3, 2, 3], # 2
-#         [2, 3, 2, 2, 1, 3, 2], # 3
-#         [1, 1, 2, 3, 2, 3, 3], # 4
-#         [1, 3, 2, 4, 1, 2, 4], # 5
-#         [4, 3, 1, 3, 1, 4, 4], # 6
-#         [3, 2, 3, 2, 1, 1, 4], # 7
-#     ])
+    # Different board shape.
+    b2 = Board(num_rows=5, num_cols=3, num_colours=4)
+    b2.board = np.array([[3, 4, 4], [5, 5, 2], [4, 4, 3], [5, 6, 2], [3, 5, 2]])
+    assert b2._get_colour_lines() == []
 
-#     print(board.get_lowest_h_match_coords())
+    # Single horizontal line on left and top edge
+    b2.board[0, 0] = 4
+    assert b2._get_colour_lines()[0] == [(0, 0), (0, 1), (0, 2)]
+    assert len(b2._get_colour_lines()) == 1
 
-#     print("original")
-#     board.print_board()
+    # Single horizontal line on bottom edge
+    b2.board[0, 2] = 6
+    b2.board[4, 0] = 5
+    b2.board[4, 2] = 5
+    assert b2._get_colour_lines()[0] == [(4, 0), (4, 1), (4, 2)]
+    assert len(b2._get_colour_lines()) == 1
+
+    # Two horizontal lines on different lines
+    b2.board = np.array([[3, 4, 4], [5, 5, 5], [4, 4, 3], [3, 3, 3], [3, 5, 2]])
+
+    assert len(b2._get_colour_lines()) == 1, b2._get_colour_lines()
+    assert b2._get_colour_lines()[0] == [(3, 0), (3, 1), (3, 2)]
+
+    # Separate horizontal and vertical lines on separate rows.
+    b2.board = np.array([[3, 4, 4], [5, 4, 5], [4, 4, 3], [3, 3, 3], [3, 5, 2]])
+
+    assert len(b2._get_colour_lines()) == 1
+    assert b2._get_colour_lines()[0] == [(3, 0), (3, 1), (3, 2)]
+
+    # Separate vertical lines on same row
+    b2.board = np.array([[3, 2, 4], [5, 4, 5], [4, 4, 3], [4, 2, 3], [4, 5, 3]])
+
+    assert len(b2._get_colour_lines()) == 2
+    assert b2._get_colour_lines()[0] == [(2, 0), (3, 0), (4, 0)]
+    assert b2._get_colour_lines()[1] == [(2, 2), (3, 2), (4, 2)]
+
+    # Line of length > 3
+    b2.board = np.array([[3, 2, 3], [2, 4, 3], [4, 4, 3], [3, 2, 3], [2, 5, 2]])
+    assert len(b2._get_colour_lines()) == 1
+    assert b2._get_colour_lines()[0] == [(0, 2), (1, 2), (2, 2), (3, 2)]
+
+    # Separate vertical lines on same row of different lengths.
+    b2.board = np.array([[3, 2, 3], [4, 4, 3], [4, 4, 3], [4, 2, 3], [2, 5, 2]])
+    assert len(b2._get_colour_lines()) == 2
+    assert b2._get_colour_lines()[0] == [(1, 0), (2, 0), (3, 0)]
+    assert b2._get_colour_lines()[1] == [(0, 2), (1, 2), (2, 2), (3, 2)]
