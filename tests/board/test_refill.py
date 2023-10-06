0b22a1f6035e0e6d565bd88228c99c91c8dd0768
akshilpatel <akshilpatel11@gmail.com>
2023-10-06 12:07:41 +0100

    Remove a redundant check in test_refill

--- tests/board/test_refill.py
+++ tests/board/test_refill.py
@@ -7,10 +7,6 @@ def test_refill():
     board.num_colours = 4
     wipe_coords(board, [(0, 0), (0, 1), (0, 3), (0, 6)])
     board.refill()
-    assert 0 not in board.board
-    assert set(board.board.flatten().tolist()).issubset(
-        set(range(board.num_colourless_specials + 1, board.num_colourless_specials + board.num_colours))
-    ), board.board
     assert np.all(board.board > 0)
     for i in range(board.num_rows):
         for j in range(board.num_cols):
