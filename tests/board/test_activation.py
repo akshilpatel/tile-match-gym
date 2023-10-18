74fcbcf86e5b99d801cd2e382da200d5c98ba738
akshilpatel <akshilpatel11@gmail.com>
2023-10-18 17:11:27 +0100

    Uncomment test_activation

--- tests/board/test_activation.py
+++ tests/board/test_activation.py
@@ -5,22 +5,22 @@ from tile_match_gym.board import Board
 from tile_match_gym.utils import print_board_diffs
 from typing import Optional, List, Tuple
 
-# def test_activate_special():
-#     # Bomb
-# 
-#     # V Laser
-# 
-#     # H Laser
-# 
-#     # Cookie
-# 
-#     # Adding to queue.
-# 
-#     # Test that a special can't be readded to queue after activation due to another tile activation
-# 
-# 
-#     # Test case that cookie is activated but surrounded already by colourless specials - current code does not work.
-#     assert False
+def test_activate_special():
+    # Bomb
+
+    # V Laser
+
+    # H Laser
+
+    # Cookie
+
+    # Adding to queue.
+
+    # Test that a special can't be readded to queue after activation due to another tile activation
+
+
+    # Test case that cookie is activated but surrounded already by colourless specials - current code does not work.
+    assert False
 
 def test_get_special_creation_pos():
     """
