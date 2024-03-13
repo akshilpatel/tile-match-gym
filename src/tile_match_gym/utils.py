4cbe50cbbb85e9d0241dc4af6f958bc323e24ead
Akshil Patel <akshilpatel11@gmail.com>
2024-03-13 17:31:48 +0000

    Add a function that computes a flat representation of the obs for tabular

--- src/tile_match_gym/utils.py
+++ src/tile_match_gym/utils.py
@@ -12,21 +12,27 @@ def compute_num_states(num_rows, num_cols, num_colours, num_processes):
 
     all_args = [(num_rows, num_cols, board, b) for b in potential_boards]
     with mp.Pool(num_processes) as p:
-        results = p.starmap(process_combo, all_args)
+        results = p.starmap(is_valid_state, all_args)
     
     r_arr = np.array(results)
 
     return r_arr[:, 0].sum(), r_arr[:, 1].sum()
 
-def process_combo(num_rows, num_cols, board, b):
+def is_valid_state(num_rows, num_cols, board, b):
     board.board[0] = np.array(b).reshape(num_rows, num_cols)
     line_matches = board.get_colour_lines()
     has_poss_move = board.possible_move()
     has_no_colour_matches = len(line_matches) == 0
     return has_poss_move and has_no_colour_matches, has_no_colour_matches
         
-
+def get_tabular_obs(board: np.ndarray, num_moves_left: int) -> tuple:
+    flat_board = board.flatten().tolist()
+    flat_board.append(num_moves_left)
+    return tuple(board)
 
 
 if __name__ == "__main__":
-    print(compute_num_states(4, 4, 3, mp.cpu_count()))
+    print(compute_num_states(4, 3, 4, mp.cpu_count()))
+
+    # (4, 3, 3) (158052, 180870)
+    # 4, 4, 3: (8426718, 8484138)
