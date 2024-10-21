4f4c27d1e01baa19fe594a3ce5b27e4e56d5d612
akshilpatel <akshilpatel11@gmail.com>
2024-10-21 13:20:40 +0100

    Version bump to v1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tile-match-gym",
-    version="1.0.4",
+    version="1.0.5",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
 )
